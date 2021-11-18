380d812748fd64e751cc1420c665591d811cf4fd
Johnny Michael Alves Faria <johnny.faria1992@gmail.com>
2021-11-18 13:46:12 -0300

    loopmusical

--- loop_musical.py
+++ loop_musical.py
@@ -1,11 +1,25 @@
 N = 0
+pico = 0
+posicao = {}
+k = 0
 while True:
-    while N not in {2:10000}:
-        N = int(input())
-        print(N)
+    N = int(input())    
     if N == 0:
         break
-print('fim do programa'+'--'*10)
+    if 2<= N <=10000:
+        vetor = (input("insira a sequencia: ").split(' '))    
+    if vetor[1]<vetor[0] and vetor[N]<vetor[0] or vetor[1]>vetor[0] and vetor[N]>vetor[0]:
+        pico+=1
+        posicao[k] = '0' 
+        k+= 1
+    for i in range(1,N+1):
+        if vetor[i-1]>vetor[i] and vetor[i+1]>vetor[i] or vetor[i-1]<vetor[i] and vetor[i+1]<vetor[i] :
+            pico+=1
+            posicao[k] = i
+            k+= 1
+       # elif vetor[i-1]<vetor[i] and vetor[i+1]<vetor[i]:
+print(vetor)
+print(vetor[0]>vetor[1])
     
 
 
