29ec024689251246c18ac063fc6ca0074a6aea72
Johnny Michael Alves Faria <johnny.faria1992@gmail.com>
2021-11-18 11:00:53 -0300

    Update not_in.py

--- not_in.py
+++ not_in.py
@@ -2,4 +2,4 @@ listamenu = (1, 2, 3, 9)
 x = 0
 while x not in (listamenu):
     print('digite 1 2 3 ou 9 para sair')
-    x = int(input('Digite um número'))
+    x = int(input('Digite um número: '))
