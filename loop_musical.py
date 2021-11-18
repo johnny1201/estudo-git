309279c668c9baaa2b771b2cddf805c7c76728f1
Johnny Michael Alves Faria <johnny.faria1992@gmail.com>
2021-11-18 18:15:19 -0300

    atualização do looping

--- loop_musical.py
+++ loop_musical.py
@@ -1,25 +1,48 @@
 N = 0
-pico = 0
-posicao = {}
-k = 0
+
 while True:
+    pico = 0
+    posicao = []
+    vetor = []
+    K = 0
     N = int(input())    
     if N == 0:
         break
     if 2<= N <=10000:
-        vetor = (input("insira a sequencia: ").split(' '))    
-    if vetor[1]<vetor[0] and vetor[N]<vetor[0] or vetor[1]>vetor[0] and vetor[N]>vetor[0]:
-        pico+=1
-        posicao[k] = '0' 
-        k+= 1
-    for i in range(1,N+1):
-        if vetor[i-1]>vetor[i] and vetor[i+1]>vetor[i] or vetor[i-1]<vetor[i] and vetor[i+1]<vetor[i] :
+        vetorstr = (input("insira a sequencia: ").split(' ')) 
+        print('seu vetor é {}'.format(vetorstr))
+    while N != len(vetor):
+        N = int(input('Defina o numero de amostras: ')) 
+        vetorstr = (input("insira uma sequencia com o numero de amostras especificado: ").split(' ')) 
+        print('sua amostra é {}'.format(vetorstr))
+    for   vetornum in vetorstr:
+        vetor.append(int(vetornum)) 
+    print('amostra transformada em inteiro\n {}'.format(vetor))
+    
+
+
+    
+    
+    for i in range(0,N-1):
+        print('vetor pos {} é {}'.format(i, vetor[i]))
+        if i==0:
+            if vetor[1]<vetor[i] and vetor[N-1]<vetor[i] or vetor[1]>vetor[i] and vetor[N-1]>vetor[i]:
+                pico+=1
+                print('pico if 1 = ',pico)
+                #posicao.append(i)  
+                K+= 1
+        elif i==N-1:
+            if vetor[N-1]>vetor[N-2] and vetor[N-1]>vetor[0] or vetor[N-2]>vetor[N-1] and vetor[N-1]<vetor[0]:
+                pico+=1
+                print('pico if 2 = ',pico)
+        elif vetor[i-1]>vetor[i] and vetor[i+1]>vetor[i] or vetor[i-1]<vetor[i] and vetor[i+1]<vetor[i] :
             pico+=1
-            posicao[k] = i
-            k+= 1
+        #    posicao[k] = i
+           
+
+    print('picos ',pico)
        # elif vetor[i-1]<vetor[i] and vetor[i+1]<vetor[i]:
-print(vetor)
-print(vetor[0]>vetor[1])
+print('acabou o programa')
     
 
 
